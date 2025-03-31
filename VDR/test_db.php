ef3fe5f9fa41a9e28c6896292caa7eb062335170
Aishwaryag28 <166582719+Aishwaryag28@users.noreply.github.com>
2025-03-31 22:18:47 +0530

    Delete VDR directory

--- VDR/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "virtual_deal_room");
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-} else {
-    echo "Database connected successfully!";
-}
-?>
